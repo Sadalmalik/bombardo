91dca0277a57b6c1a7ccaa7979484c885d1808f1
Gleb Kuzmin <gleb.kuzmin@geeky.house>
2023-02-16 00:58:07 +0100

    Add script

---
+++ Lang/.update_bin.bat
@@ -0,0 +1,5 @@
+@ECHO.OFF
+
+@COPY "..\Core\BombardoApp\bin\Debug\BombardoApp.exe" ".\bin\BombardoApp.exe"
+@COPY "..\Core\BombardoApp\bin\Debug\BombardoV1.dll" ".\bin\BombardoV1.dll"
+@COPY "..\Core\BombardoApp\bin\Debug\BombardoV2.dll" ".\bin\BombardoV2.dll"
